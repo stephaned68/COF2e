bbc37ea3aedc89dc145656d3b45e0b0d41a87a46
Stéphane Devouard <sdevouard@gmail.com>
2024-11-20 11:33:33 +0100

    Implemented a script to merge cof2e.js with HTML

---
+++ watch.js
@@ -0,0 +1,39 @@
+import { watch, readFileSync, writeFileSync } from 'fs';
+
+const cof2ehtml = "./cof2e.html";
+const cof2edist = "./cof2e-dist.html";
+const cof2ejs = "./cof2e.js";
+
+console.log(`Watching for file changes on ${cof2ejs}`);
+
+let fsWait = false;
+watch(cof2ejs, (event, filename) => {
+  if (filename) {
+    if (fsWait) return;
+    fsWait = setTimeout(() => {
+      fsWait = false;
+    }, 100);
+    console.log(`${filename} file Changed`);
+  }
+  const js = readFileSync(filename).toString().split("\r\n");
+
+  let foundSW = false;
+  const htmlRows = readFileSync(cof2ehtml)
+    .toString()
+    .split("\r\n")
+    .filter(hrow => {
+      if (hrow.startsWith("<script"))
+        foundSW = true;
+      return !foundSW;
+    });
+  
+    htmlRows.push("<script type=\"text/worker\">");
+    htmlRows.push("");
+    js.forEach(jsRow => {
+      htmlRows.push(jsRow);
+    });
+    htmlRows.push("</script>");
+
+    writeFileSync(cof2edist, htmlRows.join("\r\n"));
+    console.log(`${cof2edist} updated`);
+});
